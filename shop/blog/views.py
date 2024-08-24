009ea6055ffa91e76a8e2424c6448e06741d4c25
ars <iiforex@mail.ru>
2024-08-24 18:44:14 +0300

    new changes_22

--- shop/blog/views.py
+++ shop/blog/views.py
@@ -2,10 +2,20 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from blog.serializers import *
 from blog.models import *
+from django.shortcuts import get_object_or_404
 
 
 class BlogView(APIView):
     def get(self, request, format=None):
         blogs = Blog.objects.all()
         serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+
+
+class BlogDetailView(APIView):
+    def get(self, request, slug, format=None):
+        # Найти блог по slug
+        print(slug)
+        blog = get_object_or_404(Blog, slug=slug)
+        serializer = BlogSerializer(blog)
         return Response(serializer.data)
